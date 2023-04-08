ba01291ef780626c9616dfcb553ceea7800fc37b
refilwe204 <nqatyelwaangelin@gmail.com>
2023-04-08 23:00:32 +0200

    commit to IWA11

---
+++ scripts.js
@@ -0,0 +1,33 @@
+const items = ['biscuits', 'donuts', 'pancakes'];
+
+let outstanding = {
+  biscuits: 0,
+  donuts: 0,
+  pancakes: 0
+};
+
+let delivered = {
+  biscuits: 0,
+  donuts: 0,
+  pancakes: 0
+};
+
+const orders = document.querySelectorAll('dl');
+
+for (let i = 0; i < orders.length; i++) {
+  const order = orders[i];
+  const isDelivered = order.getAttribute('data-delivered') === 'true';
+
+  items.forEach(item => {
+    const count = parseInt(order.getAttribute(`data-${item}`));
+    if (isDelivered) {
+      delivered[item] += count;
+    } else {
+      outstanding[item] += count;
+    }
+  });
+}
+
+console.log('Outstanding:', outstanding);
+
+  
