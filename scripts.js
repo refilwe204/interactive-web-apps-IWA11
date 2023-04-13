ca293ddc75167700272a0f1cb8d5fe4b646cc44f
refilwe204 <nqatyelwaangelin@gmail.com>
2023-04-13 10:19:59 +0200

    final commit

--- scripts.js
+++ scripts.js
@@ -1,32 +1,35 @@
-const items = ['biscuits', 'donuts', 'pancakes'];
+// script.js
 
-let outstanding = {
-  biscuits: 0,
-  donuts: 0,
-  pancakes: 0
-};
+const order1 = document.querySelector('[data-key="order1"]');
+const order2 = document.querySelector('[data-key="order2"]');
+const order3 = document.querySelector('[data-key="order3"]');
 
-let delivered = {
-  biscuits: 0,
-  donuts: 0,
-  pancakes: 0
-};
+const biscuits1 = order1.querySelector('.biscuits .count');
+const donuts1 = order1.querySelector('.donuts .count');
+const pancakes1 = order1.querySelector('.pancakes .count');
+const status1 = order1.querySelector('.status dd');
 
-const orders = document.querySelectorAll('dl');
+const biscuits2 = order2.querySelector('.biscuits .count');
+const donuts2 = order2.querySelector('.donuts .count');
+const pancakes2 = order2.querySelector('.pancakes .count');
+const status2 = order2.querySelector('.status dd');
 
-for (let i = 0; i < orders.length; i++) {
-  const order = orders[i];
-  const isDelivered = order.getAttribute('data-delivered') === 'true';
+const biscuits3 = order3.querySelector('.biscuits .count');
+const donuts3 = order3.querySelector('.donuts .count');
+const pancakes3 = order3.querySelector('.pancakes .count');
+const status3 = order3.querySelector('.status dd');
 
-  items.forEach(item => {
-    const count = parseInt(order.getAttribute(`data-${item}`));
-    if (isDelivered) {
-      delivered[item] += count;
-    } else {
-      outstanding[item] += count;
-    }
-  });
-}
+biscuits1.textContent = order1.dataset.biscuits;
+donuts1.textContent = order1.dataset.donuts;
+pancakes1.textContent = order1.dataset.pancakes;
+status1.textContent = order1.dataset.delivered === "true" ? "Delivered" : "Pending";
 
-console.log('Outstanding:', outstanding);
-console.log('Delivered:', delivered);
+biscuits2.textContent = order2.dataset.biscuits;
+donuts2.textContent = order2.dataset.donuts;
+pancakes2.textContent = order2.dataset.pancakes;
+status2.textContent = order2.dataset.delivered === "true" ? "Delivered" : "Pending";
+
+biscuits3.textContent = order3.dataset.biscuits;
+donuts3.textContent = order3.dataset.donuts;
+pancakes3.textContent = order3.dataset.pancakes;
+status3.textContent = order3.dataset.delivered === "true" ? "Delivered" : "Pending";
